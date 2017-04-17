d9e2477b0f19315ff05d817569c6201f62d3ae81
Vincent Cheval <vincent.cheval@icloud.com>
2017-04-17 21:18:27 +0200

    Added some text in index.html

--- Source/core_library/config.ml
+++ Source/core_library/config.ml
@@ -1,6 +1,6 @@
 exception Internal_error
 
-let debug_activated = true
+let debug_activated = false
 
 let internal_error msg =
   Printf.printf "Internal error : %s\nPlease report the bug to vincent.cheval@loria.fr with the input file and output\n" msg;
@@ -11,7 +11,7 @@ let debug =
   then fun f -> f ()
   else fun _ -> ()
 
-let test_activated = true
+let test_activated = false
 
 let test =
   if test_activated
