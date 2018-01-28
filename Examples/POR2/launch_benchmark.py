e9f08cb468af741313fdee021d9f53ee4115d7a2
Lucca Hirschi <lucca.hirschi@inf.ethz.ch>
2018-01-28 18:29:57 +0100

    Script for launching benchmarks

---
+++ Examples/POR2/launch_benchmark.py
@@ -0,0 +1,155 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+import os
+import sys
+import glob                     # for regexp
+from time import clock, time
+import subprocess
+from datetime import datetime
+import shlex
+import argparse
+import resource
+
+import data
+import utils
+
+DEFAULT_CORES = 10
+
+def sortSize(listEx):
+   listAssoc = [(int(ex.split(".dps")[0].split("_")[-1]), ex) for ex in listEx]
+   listSorted = sorted(listAssoc, cmp=lambda (x1,x2),(y1,y2): cmp(x1, y1))
+   return([ex for (nb, ex) in listSorted])
+
+def defLimit():
+   # Set maximum CPU time to 1 second in child process, after fork() but before exec()
+   print "Setting resource limit in child (pid %d)" % os.getpid()
+   maxRealTime = 2*3600              # 2 hours
+   cpu_limit = DEFAULT_CORES*maxRealTime
+   cpu_limit = maxRealTime
+   # Limit of Memory usage (in bytes)
+   mem_limit = 10*1000*1000          # 10 GO
+   resource.setrlimit(resource.RLIMIT_CPU, (cpu_limit,cpu_limit))
+   # resource.setrlimit(resource.RLIMIT_AS, (mem_limit,mem_limit))
+   
+def main():
+    # PARSING ARGSSS
+    parser = argparse.ArgumentParser(description='Launch some benchmarks om DeepSec.')
+    parser.add_argument('-f', '--file_log',
+                        help='you can choose a name of the log file')
+    parser.add_argument('-v', '--version', nargs='*',
+                        help='you can choose the version of POR (none, old POR, new POR) [none,old,new]')
+    parser.add_argument('-ft', '--filter_tests',
+                        help='you can filter tests by giving a substrings')
+    parser.add_argument('-d', '--distributed', nargs='*',
+                        help='distribute the computation among given numbers of workers (default=10), choose 0 for no distribution')
+
+    args = parser.parse_args()
+
+    nameFile = "BENCH_results"
+    if args.file_log:
+        nameFile = args.file_log
+    log_all = open("log/" + nameFile + ".log", "a")
+    def print_all(s):
+#        print s
+        log_all.write(s)
+        log_all.flush()
+    def pprint_all(s):
+        print s
+        log_all.write(s)
+        log_all.flush()
+    def new(s):
+        return(not("OLD/" in s))
+
+    list_tests = glob.glob('*/*.dps')
+    list_tests = filter(lambda s : new(s), list_tests)
+    bina_default = '../../deepsec  -deepsec_dir ../../ '
+    if not(args.distributed):
+       bina_default += "-distributed " + str(DEFAULT_CORES) + " "
+    elif int(args.distributed[0]) <= 0:
+       pass
+    else:
+       bina_default += "-distributed " + str(args.distributed[0]) + " " 
+    if not(args.version):
+        args.version = ["old","new","none"]
+    if args.version or args.difficulty:
+        if args.version:
+            print(args.version)
+            list_binaries = []
+            if "none" in args.version:
+                list_binaries.append(bina_default + "-no_por ")
+            if "old" in args.version:
+                list_binaries.append(bina_default)
+            if "new" in args.version:
+                bina = bina_default + "-with_por_gen "
+                list_binaries.append(bina)
+
+    if args.filter_tests:
+       list_tests = filter(lambda s: args.filter_tests in s, list_tests)
+
+       # # We consider versions of tests where agents are different in each session only if we add '_diff ' to the filter.
+       # if "_diff" in args.filter_tests:
+       #    pattern = args.filter_tests.split("_diff")[0]
+       #    list_tests = filter(lambda s: pattern in s and "diff" in s, list_tests)
+       # else:
+       # if args.filter_tests == "Simple":
+       # if args.filter_tests == "WMF" or args.filter_tests == "WMF_diff":
+       #    list_tests = sortWMF(list_tests)
+       # if args.filter_tests == "AKA":
+       #    list_tests = sortAKA(list_tests)
+
+    list_tests = sortSize(list_tests)
+       
+    pprint_all("="*15 + " STARTING A NEW BENCHMARK " + "="*15 +"\n")
+    pprint_all("Date: " + str(datetime.now()) + "\n")
+    if not(args.version):
+        print("You have used no option, are you sure? Look at the helping message above.")
+    pprint_all("You chose those versions: " + str(args.version) + "\n" +
+               "On all those examples: " + str(list_tests) + "\n")
+    raw_input("Press Enter to launch all benchmarks...")
+
+   # BENCHMARKS
+    HEAD = " " + "#"*10 + " "
+    HEADA = " " + "-"*3 + " "
+    IND = " " * 50
+
+    # defLimit()                  # limit of CPU and memory usage
+    list_binaries.sort()
+    for binary in list_binaries:
+        b_name = binary.split('../')[1]
+        pprint_all("\n" + HEAD + "Starting a benchmark version: " + b_name + HEAD)
+        log_all.write("\n")
+        pprint_all(IND + str(datetime.now()) + "\n")
+        for file in list_tests:
+            killed = True
+            t_name = file.split("/")[-1].split(".dps")[0]
+            pprint_all(HEADA + "Benchmark of Protocol: " + t_name + HEADA)
+            log_all.write("\n")
+            pprint_all(IND + str(datetime.now()) + "\n") # timestamp
+            log_t_b = open("log/byFiles/" + t_name + "_" + b_name + ".log", "w+")
+            log_t_b.write(IND + str(datetime.now()))
+            args = shlex.split(binary + " " +  file)
+            print(args)
+            proc = subprocess.Popen(args,
+                                    stdout=subprocess.PIPE,
+                                    preexec_fn=defLimit())
+            for line in iter(proc.stdout.readline,''):
+                line_t = line.rstrip()
+                if ("Query" in line_t) or ("Running" in line_t) or ("s." in line_t) or ("[G-POR]" in line_t and ("traces" in line_t or "Stats" in line_t)):
+                    if "Query" in line_t:
+                       killed = False
+                    print_all(line_t + "\n")
+                    print(line_t)
+                else:
+                    # print(line_t)
+                    pass
+                log_t_b.write(line_t + "\n")
+                log_t_b.flush()
+            if killed:
+               print_all("[[KILLED]] because of timeout or memory consumption >10GO." + "\n")
+               print("[[KILLED]] because of timeout or memory consumption >10GO.")               
+            log_t_b.write("\n")
+            pprint_all("\n")
+        log_t_b.write("\n")
+        pprint_all("\n")
+
+main()
---
+++ Examples/POR2/utils.py
@@ -0,0 +1,187 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+import os
+import sys
+import glob                     # for regexp
+from time import clock, time
+import subprocess
+from datetime import datetime, timedelta
+import shlex
+import argparse
+import pprint
+import logging
+import math
+import marshal
+import re
+
+import dateutil.parser
+from texttable import *
+from tabulate import tabulate
+
+import data_gen
+
+# SETTINGS FOR PRETTY PRINTINGS
+# First Column' width
+firstWidth = 22
+# Others Columns' width
+width = 13
+
+
+def extractBench(text):
+    lastBench = text.split("=============== STARTING A NEW BENCHMARK ===============")[-1]
+    return(lastBench)
+
+def extractVers(text):
+    SEP = "##########"
+    if not(SEP in text):
+        return []
+    listVers = text.split(SEP)[1:]
+    listVers2 = []
+    i = 0
+    while(i <len(listVers)):
+        vers = listVers[i].split(": ")[1]
+        benchVers = listVers[i+1]
+        listVers2.append((vers, benchVers))
+        i = i + 2
+    return(listVers2)
+
+def extractTests(text):
+    SEP = "---"
+    if not(SEP in text):
+        return []
+    listTests = text.split(SEP)[1:]
+    listTests2 = []
+    i = 0
+    while(i <len(listTests)):
+        vers = listTests[i].split(": ")[1]
+        benchTests = listTests[i+1]
+        listTests2.append((vers, benchTests))
+        i = i+2
+    return(listTests2)
+
+def findVers(call, dicoVersions):
+    res = {}
+    resKey = ""
+    for versKey in dicoVersions:
+        vers = dicoVersions[versKey]
+        if (vers["call"].strip() == call.strip()):
+            res = vers
+            resKey = versKey
+    return(resKey)
+
+def findTest(fileName, dicoTests):
+    res = {}
+    resKey = ""
+    listNb = re.findall(r'\d+', fileName)
+    if listNb != []:
+        nb = listNb[-1]
+    else:
+        nb = 0
+    for testKey in dicoTests:
+        test = dicoTests[testKey]
+        if (test["file"].strip() == fileName.strip()):
+            res = test
+            resKey = testKey
+            return(resKey)
+        if (test["file"].strip().replace("XX", str(nb)) == fileName.strip()):
+            res = test
+            resKey = testKey
+            return(resKey)
+
+
+def printLatexMatrix(matrix):
+    return(tabulate(matrix[1:], matrix[0], tablefmt="latex"))
+
+def pprintMatrix(matrix):
+    lm = len(matrix[0])-1
+    table = Texttable()
+    # table.set_cols_align(["l", "r", "c"])
+    # table.set_deco(Texttable.HEADER)
+    table.set_deco(Texttable.BORDER | Texttable.HEADER)
+    table.set_precision(2)
+    table.set_cols_width([firstWidth]+ ([width]*lm))
+    table.set_cols_align(["l"] + (["c"]*lm))
+    table.set_cols_dtype(['t'] +  # text 
+                         (['t']*lm)) # automatic
+    # table.set_cols_valign(["t", "m", "b"])
+    table.add_rows(matrix)
+    return(table.draw())
+
+def prettyFloat(f):
+    return("%.2E" % f)
+
+def extractResults(dicoV, sortedV, dicoT, keyT, disp=None):
+    # First column of the line:
+    res = [keyT]
+    for keyV in sortedV:
+        versionDico = dicoV[keyV]
+        versionBenchs = versionDico["benchs"]
+        found = False
+        for bench in versionBenchs:
+            if (not(found) and
+                versionBenchs[bench]["file"].strip() == dicoT[keyT]["file"].strip()):
+                #res.append((versionBenchs[bench]["time"], versionBenchs[bench]["nbExplo"]))
+                if versionBenchs[bench]["res"] != dicoT[keyT]["res"]:
+                    res.append("> X <")
+                elif versionBenchs[bench]["new"]:
+                    if disp:
+                        res.append("->" + prettyFloat(versionBenchs[bench]["nbExplo"]) + "<-")
+                    else:
+                        res.append("->" + prettyFloat(versionBenchs[bench]["time"]) + "<-")
+                elif not(None == versionBenchs[bench].get("killed")) and versionBenchs[bench]["killed"]:
+#                    res.append(">(" + prettyFloat(versionBenchs[bench]["time"]) + ")")
+                    res.append(">(NonTerm)")
+                elif dateutil.parser.parse(versionBenchs[bench]["date"]) > datetime.now() + timedelta(hours=-2):
+                    if disp:
+                        res.append("[" + prettyFloat(versionBenchs[bench]["nbExplo"]) + "]")
+                    else:
+                        res.append("[" + prettyFloat(versionBenchs[bench]["time"]) + "]")
+                else:
+                    if disp:
+                        res.append(prettyFloat(versionBenchs[bench]["nbExplo"]))
+                    else:
+                        res.append(prettyFloat(versionBenchs[bench]["time"]))
+                found = True
+        if not(found):
+            res.append(".")
+    return(res)
+
+def cmpGraph(ex1, ex2):
+    if "Graph" in ex1 and "Graph" in ex2:
+        n1 = (int(ex1.split("Graph_")[1].split("_par")[0]))
+        n2 = (int(ex2.split("Graph_")[1].split("_par")[0]))
+        return(cmp(n1,n2))
+    else:
+        return(cmp(ex1,ex2))
+
+def fromVersToTests(dicoVersions, dicoTests, toLatex=False, vers="all", tests="all", disp=None):
+    sortedVersions = ['ref', 'comp',  'red']
+    listTestsKey = sorted(dicoTests.keys(), cmp = cmpGraph)
+    listTestsFile = map(lambda x: dicoTests[x]['file'], listTestsKey)
+    # first line of the matrix:
+    fstLine = [" / "] + sortedVersions
+    matrix = [fstLine]
+    for i in range(len(listTestsFile)):
+        if tests=="all" or (not("old" in listTestsKey[i])):
+            keyTest = listTestsKey[i]
+            fileName = listTestsFile[i]
+            listResults = extractResults(dicoVersions, sortedVersions, dicoTests, keyTest, disp=disp)
+            matrix.append(listResults)
+    if toLatex:
+        return(printLatexMatrix(matrix))
+    else:
+        return(pprintMatrix(matrix))
+
+def setNoNew(dico):
+    for versKey in dico:
+        for testKey in dico[versKey]["benchs"]:
+            dico[versKey]["benchs"][testKey]["new"] = False
+    return(dico)
+
+def filterData(path, dico):
+    fileName = path.split("/")[-1]
+    resKey = findTest(fileName, dico)
+    if resKey == None or resKey == "":
+        return(False)
+    else:
+        return(True)
