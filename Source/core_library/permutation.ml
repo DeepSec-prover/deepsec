d4969e2f9f3558db93641f06b7550ce039fc117b
irakoton <itsaka.rakotonirina@loria.fr>
2019-02-12 11:56:29 +0100

    before switch

--- Source/core_library/permutation.ml
+++ Source/core_library/permutation.ml
@@ -1,6 +1,17 @@
+(* sets of integers. We define an additional function testing if a set is a
+singleton (in which case it returns the corresponding element). *)
+module IntSet = struct
+  include Set.Make(struct type t = int let compare = compare end)
+
+  let is_singleton (s:t) : int option =
+    match choose_opt s with
+    | None -> None
+    | Some n -> if equal s (singleton n) then Some n else None
+end
+
+
 (* constraints on permutations are roughly abstracted as the sets of possible
 values for each permuted element. *)
-module IntSet = Set.Make(struct type t = int let compare = compare end)
 type constr = IntSet.t array
 
 (* the constraint Top for permutations on {1,...,n} *)
@@ -23,8 +34,20 @@ type permutations = constr * solution
 (* NB. A subfunction should do one pass and return true or false if a
 simplification was made. Then the global function would iterate it until
 fixpoint. *)
-let simplify_at_index (c:constr) (i:int) : unit = ()
-let simplify (c:constr) : unit = ()
+
+représenter dans le type constr un truc qui permet d'inverser rapidement la permutation, ça permet de détecter des simplifications
+
+let simplify_at_index (c:constr) (i:int) (v:int) : unit =
+  Array.iteri (fun j s -> if j <> i then c.(j) <- IntSet.remove v c.(j)) c
+
+let rec simplify_once (c:constr) (a:int) (b:int) : bool =
+  a <= b &&
+  match IntSet.is_singleton c.(a) with
+  | None -> simplify_once c (a+1) b
+  | Some n -> simplify_at_index c a n; true
+
+let simplify (c:constr) : unit =
+  while simplify_once c 0 (Array.length c-1) do () done
 
 
 (* TODO. a function updating a constraint with an information of the form
