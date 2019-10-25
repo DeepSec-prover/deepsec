ae03ee88e1f36274812ee13c7281c3de72741d07
Vincent Cheval <vincent.cheval@icloud.com>
2019-10-25 02:16:11 +0200

    Fix try

--- Source/distributed/worker.ml
+++ Source/distributed/worker.ml
@@ -1,8 +1,9 @@
 open Distributed_equivalence
 
 let _ =
-Sys.set_signal Sys.sigint (Sys.Signal_handle (fun _ -> ()));
-
+  Sys.set_signal Sys.sigint (Sys.Signal_handle (fun _ -> ()));
+  let exe_path = Filename.dirname Sys.executable_name in
+  Config.path_deepsec := exe_path;
   match ((input_value stdin): Distrib.worker) with
     | Distrib.Evaluator -> Distribution.WE.main ()
     | Distrib.Local_manager -> Distribution.WLM.main ()
