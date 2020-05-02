82386f9faf3ae7c56b4e78699b14ed72fadc4496
Vincent Cheval <vincent.cheval@icloud.com>
2020-05-02 15:54:53 +0200

    Adding the copyright to all files

--- Source/core_library/types.mli
+++ Source/core_library/types.mli
@@ -1,3 +1,19 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                               DeepSec                                  *)
+(*                                                                        *)
+(*               Vincent Cheval, project PESTO, INRIA Nancy               *)
+(*                Steve Kremer, project PESTO, INRIA Nancy                *)
+(*            Itsaka Rakotonirina, project PESTO, INRIA Nancy             *)
+(*                                                                        *)
+(*   Copyright (C) INRIA 2017-2020                                        *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU General Public License version 3.0 as described in the       *)
+(*   file LICENSE                                                         *)
+(*                                                                        *)
+(**************************************************************************)
+
 (** Main types on terms and recipes *)
 
 (** The type [quantifier] is associated to a variable to quantify it. {% In~\paper, the variables are quantified
--- Source/interface/simulator.ml
+++ Source/interface/simulator.ml
@@ -1,3 +1,19 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                               DeepSec                                  *)
+(*                                                                        *)
+(*               Vincent Cheval, project PESTO, INRIA Nancy               *)
+(*                Steve Kremer, project PESTO, INRIA Nancy                *)
+(*            Itsaka Rakotonirina, project PESTO, INRIA Nancy             *)
+(*                                                                        *)
+(*   Copyright (C) INRIA 2017-2020                                        *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU General Public License version 3.0 as described in the       *)
+(*   file LICENSE                                                         *)
+(*                                                                        *)
+(**************************************************************************)
+
 open Extensions
 open Types_ui
 
